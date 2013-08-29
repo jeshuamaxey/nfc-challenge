1e333fd2cf17f1aade46c75ab2a759a05e315296
Jeshua Maxey <jeshuamaxey@gmail.com>
2013-08-29 14:10:27 +0100

    added all server files and updated package.json

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
 var express = require("express");
 var http = require("http");
+var url  = require("url");
+var colors = require('colors');
+var api = require('./routes/api');
 var app = express();
 
 var port = process.env.PORT || 8080;
 
+app.configure(function () {
+  app.use(express.bodyParser());
+});
+
 // Set the public directory to /public
 app.use(express.static(__dirname + '/public'));
 
-app.listen(port, function() {
-	console.log('Server listening on port ' + port);
-});
+/*
+* API routes
+*/
+app.get("/api", api.api);
+app.get("/api/client_instance", api.clientInstance);
+app.get("/api/arrived/:id", api.arrived);
+app.get("/api/stats", api.stats);
 
+//not sure this does anything yet
+app.post("/api/username/:username", api.username);
 
-app.get("*", function(request, response) {
+/*
+*
+* INDEX PAGE DOESN'T LOAD WITH THIS - GET SOMEONE TO HAVE A LOOK
+
+app.get("/*", function(request, response) {
   response.writeHead(404, { "Content-Type": "text/plain" });
   response.end("404!");
+});
+*/
+
+/*
+* fire it up!
+*/
+app.listen(port, function() {
+	console.log('Server listening on port ' + port);
 });
---
+++ routes/api.js
@@ -0,0 +1,69 @@
+exports.api = function(request, response) {
+	response.send("API is running");
+};
+
+exports.clientInstance = function(request, response) {
+  response.send(newClientInstance());
+};
+
+exports.arrived = function(request, response) {
+	//learn to do a proper api with request parameters etc.
+	var id = request.params.id;
+	response.send({
+		id: id,
+		inTime: inTime(id)
+	});
+};
+
+exports.stats = function(request, response) {
+	var stats = {
+		"number of games in progress": clients.length
+	};
+  response.send(stats);
+};
+
+exports.username = function(request, response) {
+	var username = request.params.username;
+  response.send(stats);
+};
+
+/*
+*	game variables
+*/
+
+var game = {
+	timeLimit: 5000 //in ms
+};
+
+clients = []; //screw data bases arrays ftw
+
+/*
+*	helper functions
+*/
+
+function newClientInstance() {
+	var id = generateUniqueKey();
+	//need to consider issues of client being in different timezone to server
+	var now = new Date().getTime();
+	//store record of client locally to server
+	client = {
+		'id' : id,
+		'idLastSet': now,
+		'username': ""
+	};
+	clients.push(client);
+	console.log("A new player joined the game".inverse);
+	return client;
+}
+
+function generateUniqueKey() {
+	//eventually I'll come up with a better way to do this
+	return clients.length;
+}
+
+function inTime(id) {
+	var now = new Date().getTime();
+	console.log("ID: " + id);
+	var elapsed = now - clients[id].idLastSet;
+	return elapsed < game.timeLimit ? true : false;
+}
